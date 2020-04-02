06f153f013b8a825cb1a9a20461b96211eba1505
Gautier Bureau <gautier.bureau@gmail.com>
2020-04-02 15:36:17 +0200

    #56 Revert fix on distribution.

--- Distribution/delete_image.sh
+++ Distribution/delete_image.sh
@@ -14,21 +14,12 @@
 source ../Helper/helper.sh
 
 usage() {
-  echo -e "Usage: `basename $0` [OPTIONS]\tprogram to create a Dynawo image.
+  echo -e "Usage: `basename $0` [OPTIONS]\tprogram to delete a Dynawo image.
 
   where OPTIONS can be one of the following:
-    --name (-n) myname      image name created (default: dynawo-distribution)
-    --help (-h)             print this message.
+    --name (-n) myname     image name to delete (default: dynawo-dev)
+    --help (-h)            print this message.
 "
-}
-
-build_image() {
-  if ! `image_exists $image_name`; then
-    docker build --force-rm -t $image_name --no-cache=true .
-  else
-    echo "Image $image_name already exists. You can delete it with: ./delete_image.sh --name $image_name"
-    exit 1
-  fi
 }
 
 image_name=dynawo-distribution
@@ -51,4 +42,4 @@ while (($#)); do
   esac
 done
 
-build_image
+delete_image $image_name
