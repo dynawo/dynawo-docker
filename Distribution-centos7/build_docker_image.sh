c42b378d3d9b86353bd576c924553509ff6c1e1d
Gautier Bureau <gautier.bureau@gmail.com>
2022-01-28 15:57:58 +0100

    Add an image for centos7 distribution.

---
+++ Distribution-centos7/build_docker_image.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#
+# Copyright (c) 2022, RTE (http://www.rte-france.com)
+# See AUTHORS.txt
+# All rights reserved.
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, you can obtain one at http://mozilla.org/MPL/2.0/.
+# SPDX-License-Identifier: MPL-2.0
+#
+# This file is part of Dynawo, an hybrid C++/Modelica open source time domain simulation tool for power systems.
+#
+
+source ../Helper/helper.sh
+
+usage() {
+  echo -e "Usage: `basename $0` [OPTIONS]\tprogram to create a Dynawo image.
+
+  where OPTIONS can be one of the following:
+    --name (-n) myname      image name created (default: dynawo-distribution-centos7)
+    --help (-h)             print this message.
+"
+}
+
+build_image() {
+  if ! `image_exists $image_name`; then
+    docker build --force-rm -t $image_name --no-cache=true .
+  else
+    echo "Image $image_name already exists. You can delete it with: ./delete_image.sh --name $image_name"
+    exit 1
+  fi
+}
+
+image_name=dynawo-distribution-centos7
+
+while (($#)); do
+  case "$1" in
+    --help|-h)
+      usage
+      exit 0
+      ;;
+    --name|-n)
+      image_name=$2
+      shift 2
+      ;;
+    *)
+      echo "$1: invalid option."
+      usage
+      exit 1
+      ;;
+  esac
+done
+
+build_image
---
+++ Distribution-centos7/connect_to_container.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#
+# Copyright (c) 2022, RTE (http://www.rte-france.com)
+# See AUTHORS.txt
+# All rights reserved.
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, you can obtain one at http://mozilla.org/MPL/2.0/.
+# SPDX-License-Identifier: MPL-2.0
+#
+# This file is part of Dynawo, an hybrid C++/Modelica open source time domain simulation tool for power systems.
+#
+
+source ../Helper/helper.sh
+
+usage() {
+  echo -e "Usage: `basename $0` [OPTIONS]\tprogram to connect to a Dynawo container.
+
+  where OPTIONS can be one of the following:
+    --name (-n) myname      container name to connect to (default: dynawo-distribution-centos7)
+    --help (-h)             print this message.
+"
+}
+
+connect_to_container() {
+  if `container_exists $container_name`; then
+    if ! `container_is_running $container_name`; then
+      docker start $container_name
+    fi
+    docker exec -it $container_name bash
+  else
+    echo "You specified a container $container_name that is not created."
+    echo "List of available containers:"
+    for name in `docker ps -a --format "{{.Names}}"`; do echo "  $name"; done
+    exit 1
+  fi
+}
+
+container_name=dynawo-distribution-centos7
+
+while (($#)); do
+  case "$1" in
+    --help|-h)
+      usage
+      exit 0
+      ;;
+    --name|-n)
+      container_name=$2
+      shift 2
+      ;;
+    *)
+      echo "$1: invalid option."
+      usage
+      exit 1
+      ;;
+  esac
+done
+
+connect_to_container
